8a88ec06bce5f718876f81af97d3d3491ce1799b
ramyk <ramy.kader.rk@gmail.com>
2020-08-03 14:42:43 +0100

    fixed output formatting

--- path_solve.lua
+++ path_solve.lua
@@ -13,17 +13,19 @@ local path = solver.solve(graph.map,
                           graph.encoder[dpt],
                           graph.encoder[dst])
 local cost = 0
-for i=1,#path do
-    cost = cost + path[i][2]
+if path then
+    for i=1,#path do
+        cost = cost + path[i][2]
+    end
+    -- formatting output
+    io.write("Found Path: ")
+    io.write(graph.decoder[path[1][1]])
+    for i=2,#path do
+        io.write('--('..path[i][2]..')--')
+        io.write(graph.decoder[path[i][1]])
+    end
+    io.write("\nTotal Distance: "..cost)
+else
+    io.write("No path found!")
 end
-
--- formatting output
-io.write("Found Path: ")
-io.write(graph.decoder[path[1][1]])
-for i=2,#path do
-    io.write('--('..path[i][2]..')--'..graph.decoder[path[i][1]])
-end
-io.write('\n')
-
-io.write("Total Distance: "..cost)
 io.write('\n')
